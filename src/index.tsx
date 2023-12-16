aa3ff808c9e5de09fbd5957fce90d426a8aedd9b
MaksimKonyshev <92292165+MaksimKonyshev@users.noreply.github.com>
2023-12-16 15:17:29 +0300

    Setting React 17.0.2

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const container = document.getElementById('root');
-if (container) {
-    const root = createRoot(container);
-    root.render((
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
-    ));
-}
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root'),
+);
 
 reportWebVitals();
